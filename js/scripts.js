331124529a992d6d57e60033b3b694c6ef36de1a
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-20 15:16:14 -0800

    add spec for determining if the pizza has pepperoni

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 var PizzaPlace = {
   pizzaSizes: [12, 16, 20],
+  pizzaToppings: [],
+
+  withPepperoni: function() {
+    if(this.pizzaToppings.indexOf("pepperoni")) {
+      return true;
+    } else {
+      return false;
+    }
+  },
 
   pizzaSlice: function() {
     if(this.pizzaSize === 12) {
       return 6;
+    } else if (this.pizzaSize === 16) {
+      return 8;
+    } else if (this.pizzaSize === 20) {
+      return 10;
     } else {
       return false;
     }
-  }
-};
+   }
+ };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,25 @@ describe("PizzaPlace", function() {
       expect(testPizzaPlace.pizzaSlice()).to.equal(6);
     });
   });
+  describe("pizzaSlice", function() {
+    it("will return the number of slices for a medium pizza", function() {
+      var testPizzaPlace = Object.create(PizzaPlace);
+      testPizzaPlace.pizzaSize = 16;
+      expect(testPizzaPlace.pizzaSlice()).to.equal(8);
+    });
+  });
+  describe("pizzaSlice", function() {
+    it("will return the number of slices for a large pizza", function() {
+      var testPizzaPlace = Object.create(PizzaPlace);
+      testPizzaPlace.pizzaSize = 20;
+      expect(testPizzaPlace.pizzaSlice()).to.equal(10);
+    });
+  });
+  describe("withPepperoni", function() {
+    it("will returm true if the pizza has pepperoni", function() {
+      var testPizzaPlace = Object.create(PizzaPlace);
+      testPizzaPlace.pizzaTopping = "pepperoni";
+      expect(testPizzaPlace.withPepperoni()).to.equal(true);
+    });
+  });
 });
